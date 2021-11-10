5b3a4812e99730e95e6a196ad92fd88000fa8bd2
Robert Cock <bobcock@co.uk>
2021-11-10 17:40:40 +0100

    add bob-index.sh script for site search

---
+++ bob-index.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ -z "$@" ]; then
+        echo ""
+        echo "Syntax: site search <item>"
+        echo ""
+        exit 0
+fi
+
+/bin/bob-index -G /ftp-data/bob/ -D bob-index.db -M search -s "$@"
