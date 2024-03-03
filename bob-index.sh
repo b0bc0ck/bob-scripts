14e4030a9ef41461d01cb506bf10a152c19c23f2
Robert Cock <bobcock@co.uk>
2024-03-03 14:54:54 +0100

    move bob-index-links.sh into bob-index.sh and make it so +links in command creates symlinks

--- bob-index.sh
+++ bob-index.sh
@@ -7,4 +7,19 @@ if [[ -z "$@" ]]; then
         exit 0
 fi
 
-/bin/bob-index -G /ftp-data/bob/ -D bob-index.db -M search -s "$@"
+if [ "${1}" == "+links" ]; then
+        if [ ! -d "/site/_search_links" ]; then
+                echo "Folder /_search_links does not exist..."
+                exit 0
+        fi
+        if [ ! -d "/site/_search_links/${USER}" ]; then
+                mkdir -p "/site/_search_links/${USER}"
+        else
+                rm -rf "/site/_search_links/${USER}"
+                mkdir -p "/site/_search_links/${USER}"
+        fi
+        cd "/site/_search_links/${USER}"
+        /bin/scripts/bob-index -G /ftp-data/bob/ -D bob-index.db -M search -l ${USER} -s "${*:2}"
+else
+        /bin/scripts/bob-index -G /ftp-data/bob/ -D bob-index.db -M search -s "$@"
+fi
